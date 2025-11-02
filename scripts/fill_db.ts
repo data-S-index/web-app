b6cb9e54b0915b24e6ca90591890772b539ff815
Sanjay Soundarajan <ssoundarajan@calmi2.org>
2025-11-02 02:07:13 -0800

    wip: schema changes

---
+++ scripts/fill_db.ts
@@ -0,0 +1,83 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { PrismaClient } from "@prisma/client";
+import "dotenv/config";
+import { faker } from "@faker-js/faker";
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+  // clear all tables
+  await prisma.fujiScore.deleteMany();
+  await prisma.citation.deleteMany();
+  await prisma.userDataset.deleteMany();
+  await prisma.dataset.deleteMany();
+
+  // Fill datasets table with 1000 datasets
+  const q1: any = [];
+  for (let i = 0; i < 50; i++) {
+    q1.push({
+      title: faker.lorem.sentence(),
+      description: faker.lorem.paragraph(),
+      imageUrl: faker.image.url(),
+      publishedAt: faker.date.past(),
+      doi: `10.1000/${faker.string.nanoid(10)}`,
+      publisher: faker.company.name(),
+      publisherYear: faker.number.int({ min: 2000, max: 2025 }).toString(),
+      subjects: [faker.lorem.word(), faker.lorem.word(), faker.lorem.word()],
+      language: faker.helpers.arrayElement([
+        "en",
+        "es",
+        "fr",
+        "de",
+        "it",
+        "pt",
+        "ru",
+        "zh",
+      ]),
+      randomInt: faker.number.int(1000000),
+    });
+  }
+
+  await prisma.dataset.createMany({
+    data: q1,
+  });
+
+  const datasetIds = await prisma.dataset.findMany({
+    select: {
+      id: true,
+    },
+  });
+
+  // Fill citations table with 1000 citations
+  const q2: any = [];
+  for (let i = 0; i < 100; i++) {
+    q2.push({
+      datasetId: faker.helpers.arrayElement(datasetIds).id,
+      doi: `10.1000/${faker.string.nanoid(10)}`,
+      citedDate: faker.date.past(),
+    });
+  }
+  await prisma.citation.createMany({
+    data: q2,
+  });
+
+  // Fill fuji scores table with 100 fuji scores
+  const q3: any = [];
+  for (let i = 0; i < 50; i++) {
+    q3.push({
+      datasetId: datasetIds[i].id,
+      score: faker.number.int({ min: 60, max: 100 }),
+    });
+  }
+  await prisma.fujiScore.createMany({
+    data: q3,
+  });
+};
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
