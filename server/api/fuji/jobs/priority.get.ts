d3e84488386b7f7d26f4841ff0992c136fb4a81b
Sanjay Soundarajan <ssoundarajan@calmi2.org>
2025-12-21 19:09:37 -0800

    refactor: comment out job retrieval logic in Fuji API for future implementation

--- server/api/fuji/jobs/priority.get.ts
+++ server/api/fuji/jobs/priority.get.ts
@@ -1,33 +1,33 @@
 // Returns a list of fuji jobs to the background worker
 export default defineEventHandler(async (_event) => {
-  try {
-    const jobs = await prisma.fujiJob.findMany({
-      take: 30,
-      select: {
-        dataset: {
-          select: {
-            id: true,
-            identifier: true,
-            identifierType: true,
-          },
-        },
-      },
-    });
+  // try {
+  //   const jobs = await prisma.fujiJob.findMany({
+  //     take: 30,
+  //     select: {
+  //       dataset: {
+  //         select: {
+  //           id: true,
+  //           identifier: true,
+  //           identifierType: true,
+  //         },
+  //       },
+  //     },
+  //   });
 
-    if (!jobs) {
-      return [];
-    }
+  //   if (!jobs) {
+  //     return [];
+  //   }
 
-    const datasets = jobs.map((job) => ({
-      id: job.dataset.id,
-      identifier: job.dataset.identifier,
-      identifierType: job.dataset.identifierType,
-    }));
+  //   const datasets = jobs.map((job) => ({
+  //     id: job.dataset.id,
+  //     identifier: job.dataset.identifier,
+  //     identifierType: job.dataset.identifierType,
+  //   }));
 
-    return datasets || [];
-  } catch (error) {
-    console.error(error);
+  //   return datasets || [];
+  // } catch (error) {
+  //   console.error(error);
 
-    return [];
-  }
+  return [];
+  // 1}
 });
