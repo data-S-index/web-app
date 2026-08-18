da3d1fcc944f6680c06e217af05b8822b79ca5f7
Sanjay Soundarajan <ssoundarajan@calmi2.org>
2026-08-18 15:14:26 -0700

    chore: add more announcements

--- app/pages/announcements/[slug].vue
+++ app/pages/announcements/[slug].vue
@@ -31,28 +31,33 @@ defineOgImage("NuxtSeo.takumi", {
 </script>
 
 <template>
-  <div class="mx-auto flex w-full max-w-screen-md flex-col gap-6 px-6 py-10">
-    <UButton
-      to="/announcements"
-      icon="i-heroicons-arrow-left"
-      color="neutral"
-      variant="link"
-      class="w-fit p-0"
-    >
-      Back to Announcements
-    </UButton>
+  <div
+    class="mx-auto flex w-full max-w-screen-md flex-col gap-6 px-6 py-10"
+    v-if="post"
+  >
+    <div class="flex items-center justify-between">
+      <UButton
+        to="/announcements"
+        icon="i-heroicons-arrow-left"
+        color="neutral"
+        variant="link"
+        class="w-fit p-0"
+      >
+        Back to Announcements
+      </UButton>
 
-    <div v-if="post">
+      <time class="text-sm" :datetime="post.date">
+        {{ $dayjs(post.date).format("DD MMMM YYYY") }}
+      </time>
+    </div>
+
+    <div>
       <UPageCTA
         :title="post.title"
         :description="post.description"
         variant="naked"
         :ui="{ container: '!pb-6' }"
       />
-
-      <time class="text-muted -mt-4 text-sm" :datetime="post.date">
-        {{ $dayjs(post.date).format("DD MMMM YYYY") }}
-      </time>
 
       <UCard class="mt-6">
         <ContentRenderer
