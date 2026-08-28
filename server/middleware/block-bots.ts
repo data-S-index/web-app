7b911e31b6d041dda345145eccbc0ea1cdfe154a
Sanjay Soundarajan <ssoundarajan@calmi2.org>
2026-08-27 19:47:14 -0700

    ci: temp block

--- server/middleware/block-bots.ts
+++ server/middleware/block-bots.ts
@@ -1,8 +1,9 @@
 const BLOCKED_USER_AGENTS = [
-  "meta-externalagent/1.1 (+https://developers.facebook.com/docs/sharing/webmasters/crawler)",
+  "meta-externalagent/1.1",
   "ClaudeBot/1.0",
   "GPTBot/1.4",
   "Googlebot/2.1",
+  "YandexBot/3.0",
 ];
 
 // Max burst size per bot before it starts getting throttled
